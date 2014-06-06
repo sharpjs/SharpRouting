dbb7f08063a3b5846a9188e2f8e9e10c40388ef0
Jeff Sharp <jeff.sharp@git>
2014-06-06 13:21:07 -0500

    Add Int32Constraint

---
+++ FluentRouting.Tests/Constraints/Int32ConstraintTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Web.Routing;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace FluentRouting.Constraints
+{
+    [TestFixture]
+    public class Int32ConstraintTests
+    {
+        [Test]
+        public void Match_Missing()
+        {
+            Match(new { }).Should().BeFalse();
+        }
+
+        [Test]
+        public void Match_Unrecognized()
+        {
+            Match(new { p = true }).Should().BeFalse();
+        }
+
+        [Test]
+        public void Match_Unparseable()
+        {
+            Match(new { p = "not an Int32" }).Should().BeFalse();
+        }
+
+        [Test]
+        public void Match_Parseable()
+        {
+            Match(new { p = 42.ToString() }).Should().BeTrue();
+        }
+
+        [Test]
+        public void Match_Int32()
+        {
+            Match(new { p = 42 }).Should().BeTrue();
+        }
+
+        [Test]
+        public void ExtensionMethod()
+        {
+            var routes = new RouteCollection();
+            
+            routes.Map<RootController>(root =>
+            {
+                root.Parameter("p").Int32().IsAction("A");
+            });
+
+            routes["Root.A"].ShouldBeRoute("{p}", "Root", "A",
+                constraints: new { p = Int32Constraint.Instance });
+        }
+
+        private static bool Match(object values)
+        {
+            return Int32Constraint.Instance
+                .Match(null, null, "p", new RouteValueDictionary(values), RouteDirection.IncomingRequest);
+        }
+    }
+}
--- FluentRouting/ConstraintExtensions.cs
+++ FluentRouting/ConstraintExtensions.cs
@@ -9,7 +9,17 @@ namespace FluentRouting
     public static class ConstraintExtensions
     {
         /// <summary>
-        ///   Constraints the URL parameter to valid GUIDs.
+        ///   Constraints the URL parameter to valid <c>Int32</c> values.
+        /// </summary>
+        /// <param name="builder">The route builder.</param>
+        /// <returns>The route builder.</returns>
+        public static IParameterRouteBuilder Int32(this IParameterRouteBuilder builder)
+        {
+            return builder.WithConstraint(Int32Constraint.Instance);
+        }
+
+        /// <summary>
+        ///   Constraints the URL parameter to valid <c>Guid</c> values.
         /// </summary>
         /// <param name="builder">The route builder.</param>
         /// <returns>The route builder.</returns>
