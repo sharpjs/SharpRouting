2fd56d7202688b4048b0cb58186bbadbfce91a89
Jeff Sharp <jeff.sharp@git>
2014-07-25 14:50:47 -0500

    Add MVC 5 package to build script.

--- Build.cmd
+++ Build.cmd
@@ -1,4 +1,5 @@
 .nuget\NuGet.exe pack SharpRouting.Mvc4\SharpRouting.Mvc4.csproj -Build -OutputDirectory build -Properties Configuration=Release || goto done
+.nuget\NuGet.exe pack SharpRouting.Mvc5\SharpRouting.Mvc5.csproj -Build -OutputDirectory build -Properties Configuration=Release || goto done
 
 :done
 @pause
