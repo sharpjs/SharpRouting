0e414d3a4df6002c036df567f46ba4be15a7f701
Jeff Sharp <jeff.sharp@git>
2014-07-25 13:59:54 -0500

    checkpoint

---
+++ SharpRouting.Mvc5/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+
+// General Information
+[assembly: AssemblyTitle       ("SharpRouting.Mvc5")]
+[assembly: AssemblyDescription ("Fluent Routing API for ASP.NET MVC 5")]
+
+// Security
+//
+// All code is transparent; the entire assembly will not do anything privileged or unsafe.
+// http://msdn.microsoft.com/en-us/library/dd233102.aspx
+//
+[assembly: SecurityTransparent]
+
+// Testing
+#if DEBUG
+    [assembly: InternalsVisibleTo("SharpRouting.Tests")]
+#endif
